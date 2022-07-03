9aa8aa9e470dee60269191f82482b6270bdb2c93
Yersain <71192180+ImYersain@users.noreply.github.com>
2022-07-03 15:38:09 +0200

    feat: final backend

---
+++ backend/controllers/index.js
@@ -0,0 +1,2 @@
+export * as UserController from './UserController.js';
+export * as PostController from './PostController.js'; // если экспорты обычные и их много в файле то такие экспорты все из конкретного файла
---
+++ backend/utils/index.js
@@ -0,0 +1,2 @@
+export { default as handleValidationErrors } from './handleValidationErrors.js';
+export { default as checkAuth } from './checkAuth.js'; // если экспорт с файла один и он дефолтный то такой экспорт
