f0dec45ae8ab6406b59fa32f795f42a70fa50ae8
Yersain <71192180+ImYersain@users.noreply.github.com>
2022-07-05 23:31:12 +0200

    feat: added slices, requests(for posts and tags)

--- frontend/src/redux/slices/posts.js
+++ frontend/src/redux/slices/posts.js
@@ -1,5 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import instance from '../../axios';
 
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const { data } = await instance.get('/posts');
+    return data;
+})
+export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
+    const { data } = await instance.get('/tags');
+    return data;
+})
 
 const initialState = { 
     posts: {
@@ -15,7 +25,33 @@ const initialState = {
 const postSlice = createSlice({
     name: 'posts',
     initialState,
-    reducer: { }
+    reducer: { },
+    extraReducers: {
+        [fetchPosts.pending]: (state) => {
+            state.posts.status = 'loading';
+            state.posts.items = [];
+        },
+        [fetchPosts.fulfilled]: (state,action) => {
+            state.posts.status = 'loaded';
+            state.posts.items = action.payload;
+        },
+        [fetchPosts.rejected]: (state) => {
+            state.posts.status = 'error';
+            state.posts.items = [];
+        },
+        [fetchTags.pending]: (state) => {
+            state.tags.status = 'loading';
+            state.tags.items = [];
+        },
+        [fetchTags.fulfilled]: (state,action) => {
+            state.tags.status = 'loaded';
+            state.tags.items = action.payload;
+        },
+        [fetchTags.rejected]: (state) => {
+            state.tags.status = 'error';
+            state.tags.items = [];
+        },
+    }
 })
 
 
