049eaa58a6e9bc553f48ff345156db3660c7f6de
Yersain <71192180+ImYersain@users.noreply.github.com>
2022-07-12 00:17:15 +0200

    feat: createPost form(simple editor) + upload Images

--- frontend/src/pages/AddPost/index.jsx
+++ frontend/src/pages/AddPost/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -6,14 +6,37 @@ import SimpleMDE from 'react-simplemde-editor';
 
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from '../../redux/slices/auth';
+import { Navigate } from 'react-router-dom';
+import instance from '../../axios';
 
 export const AddPost = () => {
-  const imageUrl = '';
-  const [value, setValue] = React.useState('');
+  const isAuth = useSelector(selectIsAuth);
+  const [imageUrl, setImageUrl] = useState('');
+  const [value, setValue] = useState('');
+  const [title, setTitle] = useState('');
+  const [tags, setTags] = useState('');
+  const inputFileRef = useRef(null);
 
-  const handleChangeFile = () => {};
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await instance.post('/upload', formData);
+      setImageUrl(data.url);
+    } catch (error) {
+      console.warn(error);
+      alert('error uploding file')
+    }
+  };
 
-  const onClickRemoveImage = () => {};
+  const onClickRemoveImage = () => {
+    if(window.confirm('Are you sure want to delete image?')){
+      setImageUrl('');
+    }
+  };
 
   const onChange = React.useCallback((value) => {
     setValue(value);
@@ -33,20 +56,24 @@ export const AddPost = () => {
     }),
     [],
   );
+  
 
+  if(!window.localStorage.getItem('token') && !isAuth){
+    return <Navigate to='/' />
+  }
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
         Загрузить превью
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Удалить
-        </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Удалить
+          </Button>
+          <img className={styles.image} src={`http://localhost:7777${imageUrl}`} alt="Uploaded" />
+        </>
       )}
       <br />
       <br />
@@ -55,8 +82,16 @@ export const AddPost = () => {
         variant="standard"
         placeholder="Заголовок статьи..."
         fullWidth
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
+      <TextField 
+        classes={{ root: styles.tags }} 
+        variant="standard" 
+        placeholder="Тэги" 
+        fullWidth 
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}/>
       <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
       <div className={styles.buttons}>
         <Button size="large" variant="contained">
